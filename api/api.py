9150065691ea06bf9a497c72f327552e4c946d1b
andrwkoval <drone.kov@gmail.com>
2020-06-12 17:47:07 +0300

    basic rest logic for kafka

--- api/api.py
+++ api/api.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
-from db.cassandra import Cassandra
+# from db.cassandra import Cassandra
+from kafka import KafkaConsumer, TopicPartition
+import json
 
 app = Flask(__name__)
 
@@ -28,6 +30,27 @@ def events_by_group(group_id):
     obj = db.events_by_group(group_id)
     return jsonify(obj), 200
 
+@app.route('/statistics', methods=["GET"])
+def statistics():
+    consumer.seek_to_end(partition)
+    end = consumer.position(partition)
+    last = int(end) - 1
+    consumer.seek(partition, last)
+    end_offset = consumer.end_offsets([partition])[partition]
+    value = {}
+    for i in consumer:
+        value = i.value
+        if i.offset == end_offset - 1: break
+    
+    return jsonify({value}), 200
+
 if __name__ == "__main__":
+    partition = TopicPartition("test", 0)
+    consumer = KafkaConsumer(
+        enable_auto_commit=False,
+        auto_offset_reset="earliest", 
+        bootstrap_servers=["localhost:9092"])
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))))
+    consumer.assign([partition])
     db = Cassandra("<user>", "<password>", "<ips>", "<keyspace='meetup_net_project'>")
     app.run(debug=False)
